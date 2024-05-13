76923267ed7acc68f40aacbbab94f513319f1e85
kamilov2 <jasurkamilov7279@gmail.com>
2024-05-13 18:13:42 +0500

    f

---
+++ lesson_26.py
@@ -0,0 +1,25 @@
+
+
+
+# def choose_your(p):
+#     file_path = "log.txt"
+#     if p == 'w':
+#         file = open(file_path, "w")
+#         content = input("Faylga yozish uchun malumot kiriting va fayl ichidagi malumotlar butunlayin yangilanadi: ") 
+#         file.write(content)
+#         file.close()
+#         file = open(file_path , "r")
+#         print("Fayl ichidagi malumotlar shunga ozgardi\n",file.read())
+#         file.close()
+#     elif p == 'a':
+#         file = open(file_path, "a")
+#         content = input("malumot kiriting va men uni keyingi qatordan yozib ketaman: ")
+#         file.write(content)
+#         file.close()
+#     elif p == 'r':
+#         file = open(file_path, "r")
+#         print("Fayl ichidagi malumotlar\n",file.read())
+#         file.close()
+#     else:
+#         print("maksadni togri kiriting: ")
+# choose_your("r")
