c7d4920a25bd0527c9206cc0ad00c6b3fc3709bb
kamilov2 <jasurkamilov7279@gmail.com>
2024-05-22 18:18:27 +0500

    .

--- lesson_27.py
+++ lesson_27.py
@@ -1,23 +1,32 @@
-from bs4 import BeautifulSoup
+import uuid
+import json
 import requests
-url = "https://kun.uz"
+from bs4 import BeautifulSoup
+url = "https://kun.uz/en"
 response = requests.get(url)
+
+
 if response.status_code == 200:
     html_content = response.content
 
     soup = BeautifulSoup(html_content, 'html.parser')
     # print(soup)
     arr = soup.find_all('div')
-    # print(arr)
+    d = {}
+    print(soup)
     for i in arr:
         # print(i.get('class'))
-        if i.get('class') == ['small-news']:
-            print(i)      
+        if i.get('class') == ['container']:
+            news_id = str(uuid.uuid4()).split('-')[0]
+            # print(i.find_all('img'))            
+            # print(type(i.find_all('img')))
+            d[news_id] = {"image":list(i.find_all('img'))[0].get('src')}
+    print(d)
+    with open('news.json', 'a') as file:
+        json.dump(d, file)                   
 
 else:
     print("Failed to retrieve the webpage.")
 
 
-s = [1,2,3]
-s.append(4)
 
