10f90abbfc0d2f5a53c47cb4028b1c19fa8efe66
kamilov2 <jasurkamilov7279@gmail.com>
2024-05-24 18:08:42 +0500

    .

--- lesson_27.py
+++ lesson_27.py
@@ -16,17 +16,24 @@ if response.status_code == 200:
     print(soup)
     for i in arr:
         # print(i.get('class'))
-        if i.get('class') == ['container']:
+        if i.get('class') == ['small-news']:
             news_id = str(uuid.uuid4()).split('-')[0]
             # print(i.find_all('img'))            
             # print(type(i.find_all('img')))
-            d[news_id] = {"image":list(i.find_all('img'))[0].get('src')}
+            s = i.find_all('span')
+            text_inside_span = [span.get_text() for span in s]
+            print(text_inside_span[0])
+        
+            # d.find_all('div')
+            # print(s)
+            # if s[0].get('class') == ['small-news__content']:
+            #     s.get
+            d[news_id] = {"image":list(i.find_all('img'))[0].get('src'),"created_at":text_inside_span[0]}
     print(d)
-    with open('news.json', 'a') as file:
+    with open('news.json', 'w') as file:
         json.dump(d, file)                   
 
 else:
     print("Failed to retrieve the webpage.")
-
 
 
