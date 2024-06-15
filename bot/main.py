5e56ddc29ac2663ba5590fceb02592788ba4d342
kamilov2 <jasurkamilov7279@gmail.com>
2024-06-15 10:11:37 +0500

    .

--- bot/main.py
+++ bot/main.py
@@ -1,27 +1,20 @@
-import telebot
-import datetime
-import time
-from telebot import types
-bot_token = "7293075143:AAHcGWvUmnmK4lo4qxtXcmX6KmfvOTTlWtg"
+from telebot import TeleBot, types
+bot = TeleBot('7293075143:AAHcGWvUmnmK4lo4qxtXcmX6KmfvOTTlWtg')
 
-bot = telebot.TeleBot(bot_token)
+@bot.message_handler(commands=['start','salom','register'])
+def register(message):
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    contact_button = types.KeyboardButton("Send Contact", request_contact=True)
+    location_button = types.KeyboardButton("Send Location", request_location=True)
+    markup.add(contact_button, location_button)
+    bot.send_message(message.chat.id, "Choose an option:", reply_markup=markup)
 
-@bot.message_handler(commands=['hello','start','salom'])
-def start_command(message):   
-   
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton(text="salom")
-    button1 = types.KeyboardButton(text="salom")
-    
-    keyboard.add(button,button1)
-    
- 
-    bot.send_message(message.chat.id, "Assalomu alaykum! ", reply_markup=keyboard)
-
-@bot.message_handler(content_types=['text'])
-def send_message(message):
-    if str(message.text).startswith('https://'):
-        bot.send_message(message.chat.id, 'reklama tarkatma')
-        
+@bot.message_handler(content_types=['contact', 'location'])
+def handle_special_content(message):
+    if message.contact:
+        bot.send_message(message.chat.id, f"Received contact: {message.contact.phone_number}")
+    elif message.location:
+        bot.send_message(message.chat.id, f"Received location: {message.location.latitude}, {message.location.longitude}")
+        # bot.send_message(message.chat.id, f"GOOGLE MAP LINK https://www.google.com/maps/@{message.location.latitude},{message.location.longitude},12z?entry=ttu")
 
 bot.polling()
