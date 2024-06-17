eb64ad7083da092a9e2f787946645d0a6d7dcf2a
kamilov2 <jasurkamilov7279@gmail.com>
2024-06-17 18:04:54 +0500

    .

---
+++ bot/InlineButton.py
@@ -0,0 +1,18 @@
+from telebot import TeleBot, types
+
+bot = TeleBot('7293075143:AAHcGWvUmnmK4lo4qxtXcmX6KmfvOTTlWtg')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    button1 = types.InlineKeyboardButton("assalomu alaykum", callback_data='btn1')
+    button2 = types.InlineKeyboardButton("Kanalimizga obuna boling", url='https://kun.uz/')
+    markup.add(button1, button2)
+    bot.send_message(message.chat.id, "Choose an option:", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'btn1':
+        bot.send_message(call.message.chat.id, "va alaykum assalom")
+
+bot.polling()
--- bot/KeyboardButton.py
+++ bot/KeyboardButton.py
@@ -6,17 +6,24 @@ bot = TeleBot('7293075143:AAHcGWvUmnmK4lo4qxtXcmX6KmfvOTTlWtg') # - doimiy ulani
 @bot.message_handler(commands=['start','salom','register'])# decorator doimiy kuzatuvchi metod doimiy korsatilgan commandani kuzatadi
 def register(message):# register funksiyasi qaconki decorator orqali buyruq kelsa shundagina ishledi 
     markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)# KeyboardButton larni razmerga solish qurilmaga qarab
+    
     contact_button = types.KeyboardButton("Send Contact", request_contact=True)# Button hosil qilish telegram yozish joyidan pastgi qismida
+    
     location_button = types.KeyboardButton("Send Location", request_location=True)# Button hosil qilish telegram yozish joyidan pastgi qismida
+    
     markup.add(contact_button, location_button)# razmerlagan holatga yaratgan button larni berib yuborish
+    
     bot.send_message(message.chat.id, "Choose an option:", reply_markup=markup)# funksiyani tugashi doim bir response bilan tugashi shart malum buyruk kelgandan kegin majbur javob qaytarish kerak
 
 @bot.message_handler(content_types=['contact', 'location'])# decoerator malum bir buyruklarni kuzatish 'content_types' doimiy - 'location','contact','media','rasm' va 'matn' shaklidagi buyruklarni korsatish uchun kerak boladi va odimiy 'list' malumot turini qabul qiladi  
 def handle_special_content(message):# funksiya doimiy decoratordan malum uyruk kelganda ishledi va message ni qabul qiladi message oz ichiga hamma chatdegi yozuvlarni oziga oladi
     if message.contact:# malumot tekshirish message.contact message ozida contact degan malumot ni qabul qilib tekshirish True yoqi False qiymatini qaytaradi
+        
         bot.send_message(message.chat.id, f"Received contact: {message.contact.phone_number}")
     elif message.location:# malumot tekshirish message.location message ozida location degan malumot ni qabul qilib tekshirish True yoqi False qiymatini qaytaradi
-        bot.send_message(message.chat.id, f"Received location: {message.location.latitude}, {message.location.longitude}") # funksiya tugashi malum bir javob bilan 'message.location.latitude' - kordinat olish hisoblanib yer shari boyiga qarab beriladi, 'message.location.longitude' - kordinat olish hisoblanib yer shari eniga qarab beriladi 
+        
+        bot.send_message(message.chat.id, f"Received location: {message.location.latitude}, {message.location.longitude}") # funksiya tugashi malum bir javob bilan 'message.location.latitude' - kordinat olish hisoblanib yer shari boyiga qarab beriladi, 'message.location.longitude' - kordinat olish hisoblanib yer shari eniga qarab beriladi \
+            
 
 bot.polling()# bot polling doimiy boglanishlikni hosil qilib beradi bot. bu ozgaruvchidan chaqiradi
 
